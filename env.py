2c86111b1a63a78f4d1c59bffa1cfda245f1a70e
arch111arch <125242084+arch111arch@users.noreply.github.com>
2024-03-09 21:42:23 +0100

    Create env.py

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://cqnmzfaw:0jkjLj2dPpcMFYaWapWtwr5rkeD4iPMi@cornelius.db.elephantsql.com/cqnmzfaw"
+os.environ["SECRET_KEY"] = "bubblegum"
+os.environ["CLOUDINARY_URL"] = "cloudinary://384533176283827:kza15-DxVQDRFo4G6trRHp5i8_4@duemw0cvs"
