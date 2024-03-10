3de1afc48f020c0e2a5127bd389f264582a8d607
Gabriel Pettersson <gabriel.pettersson77@gmail.com>
2024-03-10 19:12:14 +0000

    Correct codespelling and add code to index.html ad post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -15,9 +15,9 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by(created_on)
+        comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
-        if posts.likes.filter(id=self.request.user_id).exists():
+        if post.likes.filter(id=self.request.user.id).exists():
             liked=True
 
         return render(
@@ -28,5 +28,4 @@ class PostDetail(View):
             "comments" : comments,
             "Likes" : liked
            },
-
         )
