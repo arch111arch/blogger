db902db8d6f61f66ae35c9db4afd95c66205b3b0
Gabriel Pettersson <gabriel.pettersson77@gmail.com>
2024-03-10 18:48:03 +0000

    Add postdetail to templates. Add PostDetail class to views. Add path to PostDetail in urls.py. Add href link to post_detail and slug in index.html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 class PostList(generic.ListView):
@@ -10,3 +10,23 @@ class PostList(generic.ListView):
     paginate_by = 6
     #number of posts that appear on the front page
     
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by(created_on)
+        liked = False
+        if posts.likes.filter(id=self.request.user_id).exists():
+            liked=True
+
+        return render(
+            request,
+           "post_detail.html",
+           {
+            "post" : post,
+            "comments" : comments,
+            "Likes" : liked
+           },
+
+        )
