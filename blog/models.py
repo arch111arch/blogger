ae646eaff5ed0470575902d64f8e792a63f42f51
Gabriel Pettersson <gabriel.pettersson77@gmail.com>
2024-03-10 00:01:33 +0000

    Install and add Summernote to have wysiwyg to the database

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-#from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
@@ -8,6 +6,7 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
+
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
