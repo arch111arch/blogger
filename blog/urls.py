48fcbdc9560f1ffa9925fbc5012cc0b4c8d534db
Gabriel Pettersson <gabriel.pettersson77@gmail.com>
2024-03-11 02:06:21 +0000

    Add post_like to post_detail.html. Add path to like/ and name='post_like to urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
 
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    patch('like/<slug:slug>', view.PostLike.as_view(), name='post_like')
     
 ]
