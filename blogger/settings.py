28e424bb7b6a74f18111c3fb97173ac1428ffc64
Gabriel Pettersson <gabriel.pettersson77@gmail.com>
2024-03-11 11:10:13 +0000

    Change name in procfile.

--- blogger/settings.py
+++ blogger/settings.py
@@ -32,6 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['blogger2024.herokuapp.com', 'localhost', '127.0.0.1', '8000-arch111arch-blogger-1mbwi5d87b8.ws-eu108.gitpod.io']
 
 
